c4836902d566201f6da1ac57f485fc9a3840ebde
Guilherme dos Santos Silva <santos.silva.guilherme0405@escola.pr.gov.br>
2026-04-13 10:30:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,31 +15,7 @@ function calcularEquilibrio() {
     const pld = Number(inputProducao.value); // Valor da produção
     const pre = Number(inputPreservacao.value); // Valor da preservação
     
-    // Atualiza os textos na tela// Espera a página carregar
-document.addEventListener('DOMContentLoaded', () => {
-    const btnSaibaMais = document.getElementById('btnSaibaMais');
-
-    // Scroll suave ao clicar no botão de "Conhecer Projeto"
-    btnSaibaMais.addEventListener('click', () => {
-        const sectionProjetos = document.querySelector('#projetos');
-        sectionProjetos.scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // Efeito de mudança de cor no header ao rolar a página
-    window.addEventListener('scroll', () => {
-        const header = document.querySelector('header');
-        if (window.scrollY > 50) {
-            header.style.padding = '0.5rem 5%';
-            header.style.backgroundColor = '#f4f9f4';
-        } else {
-            header.style.padding = '1rem 5%';
-            header.style.backgroundColor = 'white';
-        }
-    });
-
-    console.log("Agrinho 2026: Tecnologia e Sustentabilidade carregado com sucesso!");
-});
-
+    // Atualiza os textos na tela
     document.getElementById('val_producao').innerText = pld;
     document.getElementById('val_preservacao').innerText = pre;
 
